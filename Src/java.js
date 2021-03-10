3e250b8c6824b743fe8379e638cf1a040b37049f
Timothy Moss <tim@Timothys-MacBook-Pro.local>
2021-03-10 22:30:47 +1000

    Javascript creation and application

--- Src/java.js
+++ Src/java.js
@@ -1,9 +1,42 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+function showtext(){
+  var x = document.getElementById("test");
+  var y = document.getElementById("image"); 
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none"
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
   }
+}
+function hidetext() {
+  var x = document.getElementById("test");
+  var y = document.getElementById("image");
+  if (x.style.display === "block"){
+    x.style.display = "none"
+    y.style.display = "block"
+  }
+  else {
+    x.style.display = "block"
+    y.style.display = "none"
+  }
+}
+
+function hamburger() 
+{
+  var y = document.getElementById("hamburger")
+  var x = document.getElementById("nav");
+  if (x.style.opacity === "0") {
+    y.style.transform = "rotate(90deg)";
+    y.style.transition = "transform 750ms";
+    x.style.display = "flex"
+    x.style.opacity = 1;
+    x.style.transition = "opacity 750ms";
+  } else {
+    y.style.transform = "rotate(0deg)";
+    y.style.transition = "transform 750ms opacity 750ms";
+    x.style.opacity = 0;
+    x.style.display = "none";
+  }
+}
+
